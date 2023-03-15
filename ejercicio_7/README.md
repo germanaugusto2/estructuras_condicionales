c0624525fd5f030d037a6e6d94cc50037b493f02
sistemas <germanordonez@colegioguanenta.edu.co>
2023-03-15 07:44:41 -0500

    se mofifico ejercicio_7.py

---
+++ ejercicio_7/README.md
@@ -0,0 +1,4 @@
+Programa para determinar si dados tres números enteros, la suma de los dos
+primeros es igual al tercero.
+# diagrama de flujo
+![Diagrama de flujo](diagrama.png "diagrama de flujo" )
---
+++ ejercicio_7/ejercicio_7.py
@@ -0,0 +1,12 @@
+# input
+
+a = int(input("Digite el valor del coeficiente a:"))
+b = int(input("Digite el valor del coeficiente b:"))
+c = int(input("Digite el valor del coeficiente c:"))
+
+# prossecing
+
+if a+b==c:
+    print("la suma de los dos numeros es igual al tercero")
+else:
+    print("la suma de los dos numeros no es igual al tecero")
