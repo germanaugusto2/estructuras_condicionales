79a5f9283f639ea6a2eef39a12d5c13a022f47a2
sistemas <germanordonez@colegioguanenta.edu.co>
2023-03-15 07:51:53 -0500

    se modifico ejercicio_8.py

---
+++ ejercicio_8/README.md
@@ -0,0 +1,3 @@
+Programa que lea dos números enteros y averigüe si el uno es múltiplo del otro.
+# diagrama de flujo
+![Diagrama de flujo](diagrama.png "diagrama de flujo" )
---
+++ ejercicio_8/ejercicio_8.py
@@ -0,0 +1,7 @@
+x = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y: "))
+
+if x % y==0:
+    print("y es multiplo de x")
+else:
+    print("y no es multiplo de x")
